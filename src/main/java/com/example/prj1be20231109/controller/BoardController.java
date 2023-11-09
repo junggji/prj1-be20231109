0ae3991f5d49c324fd89ab2d114fe515f7e2b95d
junggji <calrinar@gmail.com>
2023-11-09 15:46:31 +0900

    한 게시물 보기 기능

--- src/main/java/com/example/prj1be20231109/controller/BoardController.java
+++ src/main/java/com/example/prj1be20231109/controller/BoardController.java
@@ -36,4 +36,9 @@ public class BoardController {
     public List<Board> list() {
         return service.list();
     }
+
+    @GetMapping("id/{id}")
+    public Board get(@PathVariable Integer id) {
+        return service.get(id);
+    }
 }
