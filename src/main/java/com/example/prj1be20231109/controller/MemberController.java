fe792fde35230ad6812b694ae794b2ba1edb8f1b
junggji <calrinar@gmail.com>
2023-11-10 15:34:06 +0900

    회원가입 정보 검증(validate), 응답코드 세분화

--- src/main/java/com/example/prj1be20231109/controller/MemberController.java
+++ src/main/java/com/example/prj1be20231109/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.prj1be20231109.controller;
 import com.example.prj1be20231109.domain.Member;
 import com.example.prj1be20231109.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +15,16 @@ public class MemberController {
     private final MemberService service;
 
     @PostMapping("signup")
-    public void signup(@RequestBody Member member) {
-        service.add(member);
+    public ResponseEntity signup(@RequestBody Member member) {
+        if (service.validate(member)) {
+            if(service.add(member)) {
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.internalServerError().build();
+            }
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping(value = "check", params = "id")
