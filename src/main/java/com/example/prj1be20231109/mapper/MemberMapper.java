e8599c07f23b1e3913c615d71356d05b6f72ca07
junggji <calrinar@gmail.com>
2023-11-10 15:25:23 +0900

    Email 중복 확인 기능 추가

--- src/main/java/com/example/prj1be20231109/mapper/MemberMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/MemberMapper.java
@@ -20,4 +20,11 @@ public interface MemberMapper {
         WHERE id = #{id}
         """)
     String selectId(String id);
+
+
+    @Select("""
+        SELECT email FROM member
+        WHERE email = #{email}
+        """)
+    String selectEmail(String email);
 }
